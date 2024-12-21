9e3ad273445f2d873c2910ce5a82f4ba705370fe
Thanapatpond <thanapat.konghorm@gmail.com>
2024-12-21 16:00:54 +0700

    test

--- lib/main.dart
+++ lib/main.dart
@@ -12,11 +12,34 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner : false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(), // Set MyHomePage as the home widget
-    );
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'flutter Demo',
+            style: TextStyle(color:Colors.red),
+            ),
+            leading: const Icon(Icons.add),
+            backgroundColor: Colors.purple,
+            ),
+        body: Container(
+          height: 100,
+          width: 100,
+          color: Colors.orange,
+          padding: const EdgeInsets.all(20),
+          margin: const EdgeInsets.all(20),
+          child: const Text('Hello'),
+
+        ),
+      floatingActionButton: FloatingActionButton(onPressed: (){
+        print('Hello');
+      },
+      child: const Icon(Icons.phone),
+      ),
+    ));
   }
 }
